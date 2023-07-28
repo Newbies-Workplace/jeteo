c50bfee177404f22a5dd72f50a921633aa3b20c5
pociatlo <k.podsadlo@gmail.com>
2023-07-28 13:56:04 +0200

    color changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,16 +1,13 @@
-'use client'
-import Link from "next/link";
+
 import { Navbar } from "@/components/navbar/Navbar";
 import { NavMenu } from "@/components/navmenu/NavMenu";
-import { StepButton } from "@/components/stepbutton/StepButton";
-
+import { Input } from "@/components/input/Input";
 export default function Page() {
   return (
     <>
       <Navbar>
         <NavMenu />
       </Navbar>
-      <StepButton steps={["elo","eki"]} activeStepIndex={2} onStepClicked={()=>{}}/>
       content
     </>
   );
