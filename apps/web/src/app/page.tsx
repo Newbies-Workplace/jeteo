48d3a0e0af86c6c4928c1aa1177589da434fedce
pociatlo <k.podsadlo@gmail.com>
2023-07-28 17:06:08 +0200

    background

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,16 @@
 import { Navbar } from "@/components/navbar/Navbar";
 import { NavMenu } from "@/components/navmenu/NavMenu";
+import GalaxyBackground from "@/components/galaxyBackground/GalaxyBackground";
 
 export default function Page() {
   return (
     <>
+    <GalaxyBackground>
       <Navbar>
         <NavMenu />
       </Navbar>
       content
+      </GalaxyBackground>
     </>
   );
 }
---
+++ apps/web/src/components/galaxyBackground/GalaxyBackground.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import styles from "./GalaxyBackground.module.scss";
+import PlanetViolet from "@/assets/images/planet-violet.svg"
+import PlanetBlue from "@/assets/images/planet-blue.svg"
+import SpaceFog from "@/assets/images/space-fog.png"
+import Ufo1 from "@/assets/images/ufo-1.svg"
+import Ufo2 from "@/assets/images/ufo-2.svg"
+import Ufo3 from "@/assets/images/ufo-3.svg"
+import Rocket from "@/assets/images/rocket.svg" // todo rocket svg
+import PlanetYellow from "@/assets/images/planet-yellow.svg"
+import SpaceElectricVehicle from "@/assets/images/chair.svg"
+
+interface GalaxyBackgroundProps {
+    hideStars?: boolean;
+    hideComets?: boolean;
+    hidePlanets?: boolean;
+    hideUfoSwarm?: boolean;
+    hideRocket?: boolean;
+    hideVehicle?: boolean;
+}
+
+export const GalaxyBackground: React.FC<React.PropsWithChildren<GalaxyBackgroundProps>> = (
+        { 
+            children,
+            hideStars,
+            hideComets,
+            hidePlanets,
+            hideUfoSwarm,
+            hideRocket,
+            hideVehicle
+        }
+    ) => {
+    return (
+        <div className={styles.wrapper} style={{backgroundImage: `url(${SpaceFog})`, backgroundSize: 'cover'}}>
+            {!hideStars &&
+                <>
+                    <div className={styles.stars}/>
+                    <div className={styles.stars2}/>
+                    <div className={styles.stars3}/>
+                </>
+            }
+
+            {!hideComets &&
+                <>
+                    <div className={styles.comets}>
+                        <div className={styles.comet}/>
+                        <div className={styles.comet}/>
+                        <div className={styles.comet}/>
+                    </div>
+                </>
+            }
+
+            {!hidePlanets &&
+                <>
+                    <PlanetViolet className={styles.planet}/>
+                        <div className={styles.planet2Container}>
+                        <PlanetYellow className={styles.planet2Moon}/>
+                        <PlanetBlue className={styles.planet2}/>
+                    </div>
+                </>
+            }
+
+            {!hideUfoSwarm &&
+                <>
+                    <div className={styles.ufoSwarm}>
+                        <Ufo1 className={styles.ufo}/>
+                        <Ufo2 className={styles.ufo}/>
+                        <Ufo3 className={styles.ufo}/>
+                    </div>
+                </>
+            }
+
+            {!hideRocket && <Rocket className={styles.rocket}/>}
+
+            {!hideVehicle && <SpaceElectricVehicle className={styles.vehicle}/>}
+
+            {children}
+        </div>
+    );
+};
+
+export default GalaxyBackground;
