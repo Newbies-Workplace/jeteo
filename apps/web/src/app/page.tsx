2e449ddbbd8448c558c163e01d85759be79943ce
pociatlo <k.podsadlo@gmail.com>
2023-07-28 16:03:55 +0200

    page cear

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Navbar } from "@/components/navbar/Navbar";
 import { NavMenu } from "@/components/navmenu/NavMenu";
-import { Section } from "@/components/section/Section"
+
 export default function Page() {
   return (
     <>
       <Navbar>
         <NavMenu />
       </Navbar>
-      <Section title="elo">asdsadsad</Section>
       content
     </>
   );
