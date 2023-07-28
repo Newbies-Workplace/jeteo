2299ba65383d38fe0a1fb4093067a10a31f55854
NB262 <k.podsadlo@gmailcom>
2023-07-28 12:26:37 +0200

    Button Done

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { Navbar } from "@/components/navbar/Navbar";
 import { NavMenu } from "@/components/navmenu/NavMenu";
+import Button from "@/components/Button/Button";
 
 export default function Page() {
   return (
     <>
       <Navbar>
         <NavMenu />
+        <Button></Button>
       </Navbar>
       content
     </>
