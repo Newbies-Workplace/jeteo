45b83e41451c63ec3b83e46afae4ae1224dbb764
JUtupe <jutuper@gmail.com>
2023-08-01 22:47:35 +0200

    ci fix

--- apps/web/src/components/rateProgress/RateProgress.tsx
+++ apps/web/src/components/rateProgress/RateProgress.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import styles from "./RateProgress.module.scss";
-import { Text } from "@/components/text/Text";
+import { Text } from "@/components/atoms/text/Text";
 import { Doughnut } from "react-chartjs-2";
 import { ArcElement, Chart as ChartJS, ChartOptions, Legend } from "chart.js";
 
