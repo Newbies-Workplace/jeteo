5fe3c9bbf78543ce135a82712809be1779064975
Kamil Wąsowski <kamil.wasowski@bed-booking.com>
2023-08-02 09:00:31 +0200

    Add logo and new moon

--- apps/web/src/app/hero/page.tsx
+++ apps/web/src/app/hero/page.tsx
@@ -4,92 +4,63 @@ import Image from "next/image";
 import planetViolet from "@/assets/images/planet-violet.svg";
 import planetOrange from "@/assets/images/planet-orange.svg";
 import planetPinkGreen from "@/assets/images/planet-pink-green.svg";
+import planetBlue from "@/assets/images/planet-blue.svg";
 import moon from "@/assets/images/moon-decoration.svg";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import GalaxyBackground from "@/components/molecules/galaxyBackground/GalaxyBackground";
+import { Text } from "@/components/atoms/text/Text";
 
 export default function Page() {
   return (
-    <Parallax pages={2} style={{ top: "0", left: "0" }}>
-      <ParallaxLayer offset={0} speed={0.001}>
-        <Image
-          className={styles.planetPinkGreen}
-          src={planetPinkGreen}
-          alt={"Planet Pink Green"}
-          width={85}
-          height={85}
-        />
-      </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.5}>
-        <Image
-          className={styles.planetOrange}
-          src={planetOrange}
-          alt={"Planet Orange"}
-          width={156}
-          height={156}
-        />
-      </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.75}>
-        <Image
-          className={styles.planetViolet}
-          src={planetViolet}
-          alt={"Planet Violet"}
-          width={53}
-          height={53}
-        />
-      </ParallaxLayer>
-      <ParallaxLayer
-        offset={0}
-        speed={1}
-        style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-      >
-        <Image src={moon} alt={"Moon"} className={styles.moon} />
-      </ParallaxLayer>
-      <ParallaxLayer
-        offset={1}
-        speed={1}
-        style={{ backgroundColor: "white", zIndex: 1000 }}
-      ></ParallaxLayer>
-    </Parallax>
-
-    // <GalaxyBackground hidePlanets>
-    //   <div className={styles.main}>
-    //     <div>
-    //       <Image
-    //         className={styles.planetPinkGreen}
-    //         src={planetPinkGreen}
-    //         alt={"Planet Pink Green"}
-    //         width={85}
-    //         height={85}
-    //       />
-    //       <Image
-    //         className={styles.planetOrange}
-    //         src={planetOrange}
-    //         alt={"Planet Orange"}
-    //         width={156}
-    //         height={156}
-    //       />
-    //       <Image
-    //         className={styles.planetViolet}
-    //         src={planetViolet}
-    //         alt={"Planet Violet"}
-    //         width={53}
-    //         height={53}
-    //       />
-    //     </div>
-    //
-    //     <div className={styles.logo}>
-    //       <Image
-    //         src={planetBlue}
-    //         alt={"Planet Blue"}
-    //         className={styles.planetBlue}
-    //       />
-    //       <div className={styles.logoTextContainer}>
-    //         <Text className={styles.logoText}>jeteo</Text>
-    //         <Text variant={"headM"}>Portal do dzielenia się wiedzą!</Text>
-    //       </div>
-    //     </div>
-    //     <Image src={moon} alt={"Moon"} className={styles.moon} />
-    //   </div>
-    // </GalaxyBackground>
+    <GalaxyBackground hidePlanets>
+      <Parallax pages={2}>
+        <ParallaxLayer offset={0} speed={-1.25}>
+          <Image
+            className={styles.planetPinkGreen}
+            src={planetPinkGreen}
+            alt={"Planet Pink Green"}
+            width={85}
+            height={85}
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.75}>
+          <Image
+            className={styles.planetOrange}
+            src={planetOrange}
+            alt={"Planet Orange"}
+            width={156}
+            height={156}
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-1}>
+          <Image
+            className={styles.planetViolet}
+            src={planetViolet}
+            alt={"Planet Violet"}
+            width={53}
+            height={53}
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.5}>
+          <div className={styles.logo}>
+            <Image
+              src={planetBlue}
+              alt={"Planet Blue"}
+              className={styles.planetBlue}
+            />
+            <div className={styles.logoTextContainer}>
+              <Text className={styles.logoText}>jeteo</Text>
+              <Text variant={"headM"}>Portal do dzielenia się wiedzą!</Text>
+            </div>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={1}>
+          <Image src={moon} alt={"Moon"} className={styles.moon} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={1}>
+          <div className={styles.contentOnMoon}>CCCCCCCCCCCC</div>
+        </ParallaxLayer>
+      </Parallax>
+    </GalaxyBackground>
   );
 }
