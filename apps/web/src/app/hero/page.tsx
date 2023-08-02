a81e24659de84b8b838ddba49ddb7aca42c565df
Kamil Wąsowski <kamil.wasowski@bed-booking.com>
2023-08-02 12:22:04 +0200

    Bruh

--- apps/web/src/app/hero/page.tsx
+++ apps/web/src/app/hero/page.tsx
@@ -9,12 +9,24 @@ import moon from "@/assets/images/moon-decoration.svg";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import GalaxyBackground from "@/components/molecules/galaxyBackground/GalaxyBackground";
 import { Text } from "@/components/atoms/text/Text";
+import Button from "@/components/atoms/button/Button";
+import Link from "next/link";
 
 export default function Page() {
+  //TODO trzeba zmiejszac width | hight na parallax Layer bo lipa jakaś się robi, z klikaniem bo zawsze jest dla tego komponentu width i height 100%
   return (
     <GalaxyBackground hidePlanets>
       <Parallax pages={2}>
-        <ParallaxLayer offset={0} speed={-1.25}>
+        <ParallaxLayer
+          offset={0}
+          speed={-1.25}
+          className={styles.planetPinkGreen}
+          style={{
+            pointerEvents: "none",
+            maxHeight: 85,
+            maxWidth: 85,
+          }}
+        >
           <Image
             className={styles.planetPinkGreen}
             src={planetPinkGreen}
@@ -23,7 +35,13 @@ export default function Page() {
             height={85}
           />
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={-0.75}>
+        <ParallaxLayer
+          offset={0}
+          speed={-0.75}
+          style={{
+            pointerEvents: "none",
+          }}
+        >
           <Image
             className={styles.planetOrange}
             src={planetOrange}
@@ -41,7 +59,13 @@ export default function Page() {
             height={53}
           />
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={-0.5}>
+        <ParallaxLayer
+          offset={0}
+          speed={-0.5}
+          style={{
+            pointerEvents: "none",
+          }}
+        >
           <div className={styles.logo}>
             <Image
               src={planetBlue}
@@ -53,13 +77,34 @@ export default function Page() {
               <Text variant={"headM"}>Portal do dzielenia się wiedzą!</Text>
             </div>
           </div>
+          <Link href={"/"}>
+            <Button
+              primary
+              size="small"
+              style={{
+                paddingLeft: 16,
+                paddingRight: 16,
+              }}
+              onClick={() => console.log("Button clicked")}
+            >
+              Sprawdź listę wydarzeń
+            </Button>
+          </Link>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={1}>
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          style={{
+            pointerEvents: "none",
+          }}
+        >
           <Image src={moon} alt={"Moon"} className={styles.moon} />
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={1}>
-          <div className={styles.contentOnMoon}>CCCCCCCCCCCC</div>
-        </ParallaxLayer>
+        {/* <ParallaxLayer offset={0} speed={1} style={{
+          pointerEvents: 'none'
+        }}>
+          <div className={styles.contentOnMoon}></div>
+        </ParallaxLayer> */}
       </Parallax>
     </GalaxyBackground>
   );
