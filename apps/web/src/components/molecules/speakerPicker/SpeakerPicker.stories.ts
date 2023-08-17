0c2373b2b72022862ec24fb047d167925dd9d551
Kamil Wąsowski <bazixx123@gmail.com>
2023-08-17 17:23:58 +0200

    speaker picker

--- apps/web/src/components/molecules/speakerPicker/SpeakerPicker.stories.ts
+++ apps/web/src/components/molecules/speakerPicker/SpeakerPicker.stories.ts
@@ -1,13 +1,50 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { SpeakerPicker } from "@/components/molecules/speakerPicker/SpeakerPicker";
+import Profile from "@/assets/images/default-profile-pic.svg";
 
 const meta: Meta<typeof SpeakerPicker> = {
   title: "Components/Molecules/SpeakerPicker",
   component: SpeakerPicker,
+  argTypes: {
+    onAddInvite: { action: "Add Invite" },
+    onDeleteInvite: { action: "Delete Invite" },
+    onDeleteSpeaker: { action: "Delete Speaker" },
+  },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof SpeakerPicker>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {},
+};
+
+export const WithInvites: Story = {
+  args: {
+    invites: [
+      { email: "prelegent1@test.test", name: "John Doe", link: "link" },
+      { email: "prelegent2@test.test", name: "John Doe", link: "link2" },
+    ],
+    speakers: [],
+  },
+};
+
+export const WithSpeakers: Story = {
+  args: {
+    invites: [],
+    speakers: [
+      { name: "Prelegent Jeden", avatar: Profile },
+      { name: "Prelegent Dwa", avatar: Profile },
+    ],
+  },
+};
+
+export const WithAllData: Story = {
+  args: {
+    invites: [
+      { email: "prelegent2@test.test", name: "John Doe", link: "link" },
+    ],
+    speakers: [{ name: "Prelegent Jeden", avatar: Profile }],
+  },
+};
--- apps/web/src/components/molecules/speakerPicker/SpeakerPicker.tsx
+++ apps/web/src/components/molecules/speakerPicker/SpeakerPicker.tsx
@@ -1,14 +1,40 @@
+"use client";
 import React from "react";
 import styles from "./SpeakerPicker.module.scss";
 import GreyCircle from "@/assets/images/speaker-picker-circle-grey.svg";
+import PrimaryCircle from "@/assets/images/speaker-picker-circle-primary.svg";
 import Add from "@/assets/add.svg";
+import Delete from "@/assets/delete.svg";
+import Copy from "@/assets/copy.svg";
 import Image from "next/image";
+import { Text } from "@/components/atoms/text/Text";
+import Button from "@/components/atoms/button/Button";
+import { Avatar } from "@/components/atoms/avatar/Avatar";
+import cs from "classnames";
 
-export const SpeakerPicker: React.FC = () => {
+interface SpeakerPickerProps {
+  onAddInvite?: () => void;
+  onDeleteInvite?: () => void;
+  onDeleteSpeaker?: () => void;
+  invites: { email: string; name: string; link: string }[];
+  speakers: { name: string; avatar: string }[];
+}
+
+export const SpeakerPicker: React.FC<SpeakerPickerProps> = ({
+  onAddInvite,
+  onDeleteInvite,
+  onDeleteSpeaker,
+  invites,
+  speakers,
+}) => {
+  async function copyTextToClipboard(text: string) {
+    return await navigator.clipboard.writeText(text);
+  }
+
   return (
     <div className={styles.container}>
-      <div className={styles.inputs}>
-        <Image src={GreyCircle} alt={GreyCircle} width={32} height={32} />
+      <div className={styles.record}>
+        <Image src={GreyCircle} alt={"GreyCircle"} width={32} height={32} />
 
         <input className={styles.input} type="email" placeholder="Email" />
         <input
@@ -23,11 +49,81 @@ export const SpeakerPicker: React.FC = () => {
           alt={"add"}
           width={20}
           height={20}
+          onClick={onAddInvite}
         />
       </div>
 
-      {/*todo lista zaproszen*/}
-      {/*todo lista userów*/}
+      {invites &&
+        invites.map((invite, index) => {
+          return (
+            <div
+              className={cs(styles.record, styles.items)}
+              key={`invite-${index}`}
+            >
+              <Image
+                src={PrimaryCircle}
+                alt={"PrimaryCircle"}
+                width={32}
+                height={32}
+              />
+              <Text className={styles.text} variant={"bodyM"}>
+                {invite.email}
+              </Text>
+              <Text className={styles.name} variant={"bodyM"}>
+                {invite.name}
+              </Text>
+              <Button
+                className={styles.button}
+                primary
+                size="small"
+                icon={Copy}
+                onClick={() => {
+                  copyTextToClipboard(invite.link);
+                }}
+              >
+                Skopiuj zaproszenie
+              </Button>
+              <Image
+                className={styles.delete}
+                src={Delete}
+                alt={"Delete"}
+                width={22}
+                height={22}
+                onClick={onDeleteInvite}
+              />
+            </div>
+          );
+        })}
+
+      {speakers &&
+        speakers.map((speaker, index) => (
+          <div
+            className={cs(styles.record, styles.items)}
+            key={`speaker-${index}`}
+          >
+            <Avatar src={speaker.avatar} size={32} />
+            <Text
+              className={styles.text}
+              style={{ textAlign: "start", width: "100%", marginLeft: -1 }}
+              variant={"bodyM"}
+            >
+              {speaker.name}
+            </Text>
+            <Image
+              className={styles.delete}
+              src={Delete}
+              alt={"Delete"}
+              width={22}
+              height={22}
+              onClick={onDeleteSpeaker}
+            />
+          </div>
+        ))}
+
+      <Text variant={"bodyS"} bold>
+        Maksymlanie 2 prelegentów. Linki z zaproszeniami będą aktywowane i
+        wysłane mailem po zapisaniu prelekcji.
+      </Text>
     </div>
   );
 };
