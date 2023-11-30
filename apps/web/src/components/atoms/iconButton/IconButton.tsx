06bdd6c4b3e3c0c667295de4a05855430d12931a
Filszu <filipszumowski05@gmail.com>
2023-11-30 15:30:35 +0100

    fix commit

--- apps/web/src/components/atoms/iconButton/IconButton.tsx
+++ apps/web/src/components/atoms/iconButton/IconButton.tsx
@@ -18,14 +18,6 @@ export const IconButton: React.FC<IconButtonProps> = ({
   onClick,
   primary = false,
   size = "medium",
-<<<<<<< HEAD
-  
-  // size=24,
-  // btnSize=40,
-
-
-=======
->>>>>>> main
 }) => {
   const imageSize = size === "medium" ? 24 : 18;
 
@@ -35,24 +27,14 @@ export const IconButton: React.FC<IconButtonProps> = ({
         [styles.primary]: primary,
         [styles.medium]: size === "medium",
         [styles.small]: size === "small",
-<<<<<<< HEAD
-      })}    >
-        
-=======
       })}
     >
->>>>>>> main
       <Image
         src={icon}
         alt={alt ?? "iconButton"}
         onClick={onClick}
-<<<<<<< HEAD
-        width={size==="medium"?24:size==="small"?18:10}
-        height={size==="medium"?24:size==="small"?18:10}
-=======
         width={imageSize}
         height={imageSize}
->>>>>>> main
       />
     </div>
   );
