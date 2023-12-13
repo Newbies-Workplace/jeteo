f4c7d973234a4faaa5b8c7c5d7db293eca56ce11
Kamil Wąsowski <kamil.wasowski00@gmail.com>
2023-12-13 22:48:34 +0100

    feat/logout (#100)
    * logout
    
    * fix ci
    
    * cleanup
    
    * fix multiple fetchUser @me
    
    * fix /null requests
    
    * fix

--- apps/web/src/contexts/Auth.context.tsx
+++ apps/web/src/contexts/Auth.context.tsx
@@ -7,11 +7,13 @@ import { myFetch } from "@/common/fetch";
 
 interface AuthContext {
   fetchUser: () => Promise<void>;
+  logout: () => void;
   user: UserDetailsResponse | null;
 }
 
 export const AuthContext = createContext<AuthContext>({
   fetchUser: async () => {},
+  logout: async () => {},
   user: null,
 });
 
@@ -19,21 +21,25 @@ export const AuthContextProvider: React.FC<any> = ({ children }) => {
   const [user, setUser] = useState<UserDetailsResponse | null>(null);
 
   useEffect(() => {
-    if (!Cookies.get("token") && !user) {
+    if (!!Cookies.get("token") && !user) {
       fetchUser();
     }
-  }, []);
+  }, [user]);
 
   const fetchUser = async () => {
     const user: UserDetailsResponse = await myFetch("/rest/v1/users/@me").then(
       (res) => res.json()
     );
-
     setUser(user);
   };
 
+  const logout = () => {
+    Cookies.remove("token");
+    window.location.reload();
+  };
+
   return (
-    <AuthContext.Provider value={{ user, fetchUser }}>
+    <AuthContext.Provider value={{ user, fetchUser, logout }}>
       {children}
     </AuthContext.Provider>
   );
