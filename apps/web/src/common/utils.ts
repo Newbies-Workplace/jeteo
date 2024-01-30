cc4a0c2bf6b8aeb20affc04dfb2d38afc7b6ea1b
Wiktor Petryszyn <33636616+JUtupe@users.noreply.github.com>
2024-01-30 21:21:58 +0100

    fix: timezones v2 (#109)
    * fix: timezones
    
    * fix: timezones

--- apps/web/src/common/utils.ts
+++ apps/web/src/common/utils.ts
@@ -9,12 +9,8 @@ dayjs.extend(timezone);
 export const formatFromToDates = (from: string, to: string): string => {
   dayjs.locale("pl");
 
-  const fromDate = dayjs
-    .utc(from)
-    .tz(Intl.DateTimeFormat().resolvedOptions().timeZone);
-  const toDate = dayjs
-    .utc(to)
-    .tz(Intl.DateTimeFormat().resolvedOptions().timeZone);
+  const fromDate = dayjs.utc(from).tz("Europe/Warsaw");
+  const toDate = dayjs.utc(to).tz("Europe/Warsaw");
 
   if (fromDate.isSame(toDate, "day")) {
     return `${fromDate.format("D MMMM YYYY, HH:mm")} do ${toDate.format(
@@ -30,9 +26,7 @@ export const formatFromToDates = (from: string, to: string): string => {
 export const formatStartDate = (from: string): string => {
   dayjs.locale("pl");
 
-  const fromDate = dayjs
-    .utc(from)
-    .tz(Intl.DateTimeFormat().resolvedOptions().timeZone);
+  const fromDate = dayjs.utc(from).tz("Europe/Warsaw");
 
   return `${fromDate.format("D MMMM YYYY, HH:mm")}`;
 };
