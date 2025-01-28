f6cc14319f0a805a54c0a62885a6e5370d62e251
JUtupe <jutuper@gmail.com>
2025-01-29 00:17:56 +0100

    fix: ci

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -110,7 +110,9 @@ export default function RootLayout({
           />
         </Providers>
 
-        <CookieDialog />
+        <Suspense>
+          <CookieDialog />
+        </Suspense>
       </body>
     </html>
   );
